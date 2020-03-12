1dc59bd07de31860afc5cbb1b14eb3731fefb532
Ítalo Epifânio <italoepifaniols@gmail.com>
2020-03-12 11:40:35 -0300

    Resolve aproximação

--- task1/exec22/exec22.py
+++ task1/exec22/exec22.py
@@ -9,7 +9,7 @@ even = lambda k,x: (-1)**(k/2)*k*sin(x) + (-1)*(k/2) * x * cos(x)
 
 def f(x):
     result_global = 0
-    for i in range(0, 18):
+    for i in range(0, 160):
         if i%2 == 0:
             r = (even(i, a)/factorial(i))*((x - a)**i)
         else:
@@ -18,8 +18,10 @@ def f(x):
     
     return result_global
 
-rng = np.linspace(-10, 10, 100)
+rng = np.linspace(-100, 100, 1000)
 fx = [f(i) for i in rng]
 
 gp.s([rng, fx], filename='teste.out')
-gp.c('plot "teste.out"')
+gp.c('set xrange [-10:10]')
+gp.c('plot x*cos(x) + 1 title "f(x) = x*cos(x) + 1"')
+gp.c('replot "teste.out" title "aproximação" w lp ls 1 lc rgb "blue"')
