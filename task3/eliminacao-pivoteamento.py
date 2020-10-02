8f22374192e0e2ffdc34ae5532b07989f67f7abb
Ítalo Epifânio <italoepifaniols@gmail.com>
2020-10-02 00:03:07 -0300

    Melhora eliminacao pivoteamento

--- task3/eliminacao-pivoteamento.py
+++ task3/eliminacao-pivoteamento.py
@@ -1,55 +1,36 @@
 # Eliminacao de Gauss com pivoteamento
-from sys import maxsize
+def escolhePivo(matriz, i, j):
+    maior = 0
+    maiorIndice = i
+    while i < len(matriz):
+       # print(mat[i][j])
+        if abs(matriz[i][j]) > maior:
+            maior = matriz[i][j]
+            maiorIndice = i
+        i += 1
+    return maiorIndice
 
-def multiplicaLinha(linha, i):
-  return [x * i for x in linha]
-
-def subtraiLinha(linhaA, linhaB):
-  return [a - b for (a, b) in zip(linhaA, linhaB)]
-
-def escolhePivo(matriz, coluna = 0):
-    max = -maxsize - 1
-    index = 0
-    it = coluna
-    while it < len(matriz):
-        if abs(matriz[it][coluna]) > max:
-            max = matriz[it][coluna]
-            index = it
-        it += 1 
-    return index
-
-def trocaLinhas(matriz, i, j):
-    temp      = matriz[i]
-    matriz[i] = matriz[j]
-    matriz[j] = temp
-    return matriz
-
-def metodo(matriz):
+def obterMatrizTriangular(matriz):
     n = len(matriz)
-    j = 0
-    i = 0
-    while j < (n-1):
-        pivoIndex = escolhePivo(matriz, j)
-        temp = matriz[j]
-        matriz[j] = matriz[pivoIndex]
-        matriz[pivoIndex] = temp
-        i = j+1
-        while i < n:
-            m = matriz[i][j]/matriz[j][j]
-            matriz[i] = subtraiLinha(matriz[i], multiplicaLinha(matriz[j], m))
-            i += 1
-        j += 1
+    for i in range(n-1):
+        pivoIndex = escolhePivo(matriz, i, i)
+        aux = matriz[i]
+        matriz[i] = matriz[pivoIndex]
+        matriz[pivoIndex] = aux
+        for j in range(i, n-1):
+            fator = matriz[j+1][i]/matriz[i][i]
+            for k in range(0, n+1):
+                matriz[j+1][k] = matriz[j+1][k] - matriz[i][k]*fator
     return matriz
-
 
 matriz = []
-for i in range(1, 4):
+for i in range(1, 5):
     with open(f'./data/m{i}.txt', 'r') as f:
         next(f)
         matriz = [[float(num) for num in line.split(' ')] for line in f]
 
     with open(f'./data/r{i}.txt', 'w') as f:
-        for linha in metodo(matriz):
+        for linha in obterMatrizTriangular(matriz):
             for elemento in linha:
                 f.write(f'{elemento} ')
             f.write("\n")
