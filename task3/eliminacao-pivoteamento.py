8fe9125a37bda5c3ee8e34a2f255e680696f8911
Ítalo Epifânio <italoepifaniols@gmail.com>
2020-10-02 21:18:01 -0300

    Eliminação vetor normal

--- task3/eliminacao-pivoteamento.py
+++ task3/eliminacao-pivoteamento.py
@@ -26,14 +26,32 @@ def obterMatrizTriangular(matriz):
                 matriz[j+1][i] = 0
     return matriz
 
+def vetorResidual(matrizTriangular):
+    m = len(matrizTriangular)
+    result = []
+    for i in range(m - 1, -1, -1): # itera pelo fim 3, 2, 1, 0...
+        result.insert(0, matrizTriangular[i][m] / matrizTriangular[i][i])
+        for k in range(i - 1, -1, -1):
+            matrizTriangular[k][m] -= matrizTriangular[k][i] * result[0]
+    return result
+
+def norma(vetorResidual):
+    value = 0
+    for elem in vetorResidual:
+        value += pow(abs(elem), 2)
+    return pow(value, 0.5)
+
 matriz = []
-for i in range(1, 5):
+for i in range(1, 4):
     with open(f'./data/m{i}.txt', 'r') as f:
         next(f)
         matriz = [[float(num) for num in line.split(' ')] for line in f]
 
     with open(f'./data/r{i}.txt', 'w') as f:
-        for linha in obterMatrizTriangular(matriz):
+        matrizTriangular = obterMatrizTriangular(matriz)
+        normaVetor       = vetorResidual(matrizTriangular)
+        print(normaVetor)
+        for linha in matrizTriangular:
             for elemento in linha:
                 f.write(f'{elemento} ')
             f.write("\n")
